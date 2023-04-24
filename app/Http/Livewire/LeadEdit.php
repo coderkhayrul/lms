e3c5ecb37880a217c42ea520f935fbd524235618
coderkhayrul <coderkhayrul@gmail.com>
2023-04-24 23:21:49 +0600

    Loding Button Added

--- app/Http/Livewire/LeadEdit.php
+++ app/Http/Livewire/LeadEdit.php
@@ -8,9 +8,38 @@ use Livewire\Component;
 class LeadEdit extends Component
 {
     public  $lead_id;
+    public $name;
+    public $email;
+    public $phone;
+
+    public function mount(){
+        $lead = Lead::findOrFail($this->lead_id);
+        $this->lead_id = $lead->id;
+        $this->name = $lead->name;
+        $this->email = $lead->email;
+        $this->phone = $lead->phone;
+    }
+    protected $rules = [
+        'email' => 'required|email',
+        'phone' => 'required',
+    ];
     public function render()
     {
         $lead = Lead::findOrFail($this->lead_id);
         return view('livewire.lead-edit', compact('lead'));
+    }
+
+    public function submitForm(){
+        sleep(2);
+        $lead = Lead::findOrFail($this->lead_id);
+        //  form validation
+        $this->validate();
+        $lead->name = $this->name;
+        $lead->email = $this->email;
+        $lead->phone = $this->phone;
+        $lead->update();
+        flash()->addSuccess('Lead updated successfully.');
+//        return redirect()->route('leads.index');
+        return redirect()->back();
     }
 }
--- resources/views/livewire/lead-edit.blade.php
+++ resources/views/livewire/lead-edit.blade.php
@@ -1,3 +1,27 @@
 <div>
-   Lead Id : {{ $lead->name }}
+    <form wire:submit.prevent="submitForm">
+        <div class="mb-4">
+            <lable class="lms-lable">Name</lable>
+            <input wire:model="name" class="lms-input" type="text" name="name">
+            @error('name') <span class="error">{{ $message }}</span> @enderror
+        </div>
+        <div class="mb-4">
+            <lable class="lms-lable">Phone</lable>
+            <input wire:model="phone" class="lms-input" type="text" name="phone">
+            @error('phone') <span class="error">{{ $message }}</span> @enderror
+        </div>
+        <div class="mb-4">
+            <lable class="lms-lable">Email</lable>
+            <input wire:model="email" class="lms-input" type="email" name="email">
+            @error('email') <span class="error">{{ $message }}</span> @enderror
+        </div>
+
+        <div wire:loading class="flex align-middle">
+            @include('components.icons.loading')
+            Loading...
+        </div>
+        <button wire:loading.remove type="submit" class="p-3 border bg-green-500 text-white rounded">
+            Update
+        </button>
+    </form>
 </div>
