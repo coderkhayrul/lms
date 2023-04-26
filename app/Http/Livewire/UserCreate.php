c570c679155356dd3efe9769fac23ab7f212879f
coderkhayrul <coderkhayrul@gmail.com>
2023-04-26 16:42:51 +0600

    User Created

--- app/Http/Livewire/UserCreate.php
+++ app/Http/Livewire/UserCreate.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 use Spatie\Permission\Models\Role;
 class UserCreate extends Component
@@ -9,12 +11,32 @@ class UserCreate extends Component
     public $name;
     public $email;
     public $password;
-    public $password_confirmation;
     public $role;
+
+    protected $rules = [
+        'name' => 'required',
+        'email' => 'required|email|unique:users,email',
+        'password' => 'required',
+        'role' => 'required',
+    ];
+
     public function render()
     {
         return view('livewire.user-create',[
-            'roles' => Role::all()
+            'roles' => Role::where('name','!=','SuperAdmin')->get()
         ]);
+    }
+
+    public function submitForm()
+    {
+        $this->validate();
+        $user = User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => Hash::make($this->password),
+        ]);
+        $user->assignRole($this->role);
+        flash()->addSuccess('User Created Successfully');
+        return redirect()->route('users.index');
     }
 }
--- resources/views/livewire/user-create.blade.php
+++ resources/views/livewire/user-create.blade.php
@@ -1,42 +1,32 @@
 <div>
-    <form action="">
+    <form wire:submit.prevent="submitForm">
         <div class="flex">
             <div class="w-1/2 mr-2">
                 <label for="name">Name</label>
-                <input type="text" wire:model="name" class="w-full rounded-lg">
+                <input id="name" type="text" wire:model.lazy="name" class="w-full rounded-lg">
+                @error('name') <span class="error">{{ $message }}</span> @enderror
             </div>
             <div class="w-1/2 ml-2">
                 <label for="email">Email</label>
-                <input type="email" wire:model="email" class="w-full rounded-lg">
+                <input id="email" type="email" wire:model.lazy="email" class="w-full rounded-lg">
+                @error('email') <span class="error">{{ $message }}</span> @enderror
             </div>
         </div>
-        <div class="flex">
+        <div class="flex mb-5">
             <div class="w-1/2 mr-2">
                 <label for="password">Password</label>
-                <input type="password" wire:model="password" class="w-full rounded-lg">
+                <input id="password" type="password" wire:model.lazy="password" class="w-full rounded-lg">
+                @error('password') <span class="error">{{ $message }}</span> @enderror
             </div>
-            <div class="w-1/2 ml-2">
-                <label for="password_confirmation">Password Confirmation</label>
-                <input type="password" wire:model="password_confirmation" class="w-full rounded-lg">
-            </div>
-        </div>
-        <div class="flex mb-4">
             <div class="w-1/2 mr-2">
                 <label for="role">Role</label>
-                <select wire:model="role" class="w-full rounded-lg">
+                <select id="role" wire:model.lazy="role" class="w-full rounded-lg">
                     <option value="">Select Role</option>
                     @foreach ($roles as $role)
                         <option value="{{ $role->name }}">{{ $role->name }}</option>
                     @endforeach
                 </select>
-            </div>
-            <div class="w-1/2 ml-2">
-                <label for="status">Status</label>
-                <select wire:model="status" class="w-full rounded-lg">
-                    <option value="">Select Status</option>
-                    <option value="1">Active</option>
-                    <option value="0">Inactive</option>
-                </select>
+                @error('role') <span class="error">{{ $message }}</span> @enderror
             </div>
         </div>
         <!-- Loading State -->
