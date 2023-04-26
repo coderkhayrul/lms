e457df6e682c0f2a5068a30578eee7eb34914a32
coderkhayrul <coderkhayrul@gmail.com>
2023-04-26 15:25:57 +0600

    User Create page Design Update

--- app/Http/Livewire/UserCreate.php
+++ app/Http/Livewire/UserCreate.php
@@ -3,11 +3,18 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
-
+use Spatie\Permission\Models\Role;
 class UserCreate extends Component
 {
+    public $name;
+    public $email;
+    public $password;
+    public $password_confirmation;
+    public $role;
     public function render()
     {
-        return view('livewire.user-create');
+        return view('livewire.user-create',[
+            'roles' => Role::all()
+        ]);
     }
 }
--- resources/views/livewire/user-create.blade.php
+++ resources/views/livewire/user-create.blade.php
@@ -1,3 +1,48 @@
 <div>
-    User Create Page
+    <form action="">
+        <div class="flex">
+            <div class="w-1/2 mr-2">
+                <label for="name">Name</label>
+                <input type="text" wire:model="name" class="w-full rounded-lg">
+            </div>
+            <div class="w-1/2 ml-2">
+                <label for="email">Email</label>
+                <input type="email" wire:model="email" class="w-full rounded-lg">
+            </div>
+        </div>
+        <div class="flex">
+            <div class="w-1/2 mr-2">
+                <label for="password">Password</label>
+                <input type="password" wire:model="password" class="w-full rounded-lg">
+            </div>
+            <div class="w-1/2 ml-2">
+                <label for="password_confirmation">Password Confirmation</label>
+                <input type="password" wire:model="password_confirmation" class="w-full rounded-lg">
+            </div>
+        </div>
+        <div class="flex mb-4">
+            <div class="w-1/2 mr-2">
+                <label for="role">Role</label>
+                <select wire:model="role" class="w-full rounded-lg">
+                    <option value="">Select Role</option>
+                    @foreach ($roles as $role)
+                        <option value="{{ $role->name }}">{{ $role->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="w-1/2 ml-2">
+                <label for="status">Status</label>
+                <select wire:model="status" class="w-full rounded-lg">
+                    <option value="">Select Status</option>
+                    <option value="1">Active</option>
+                    <option value="0">Inactive</option>
+                </select>
+            </div>
+        </div>
+        <!-- Loading State -->
+        @include('components.icons.loading')
+        <button wire:loading.remove type="submit" class="lms-button">
+            Save
+        </button>
+    </form>
 </div>
